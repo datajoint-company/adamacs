7cebe5af7422e60be372b1e9ea108765b44000c6
danielmk <danielmuellermsc@gmail.com>
2021-10-06 22:01:16 +0200

    Misc

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='adamacs',
+    version='0.0.1',
+    description='Architectures for Data Management and Computational Support.',
+    long_description=readme,
+    author='Daniel Müller-Komorowska',
+    author_email='danielmuellermsc@gmail.com',
+    url='https://github.com/SFB1089/adamacs.git',
+    license=license,
+    packages=['adamacs'],
+    install_requires=[
+          'numpy',
+          'pandas',
+          'matplotlib',
+          'scipy'
+      ])
