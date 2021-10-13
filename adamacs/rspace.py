f2827df1feb7762a93dae91e841d249afd152cfd
Daniel Müller <daniel>
2021-10-13 14:22:12 +0200

    RSpace API working

--- adamacs/rspace.py
+++ adamacs/rspace.py
@@ -1,6 +1,16 @@
 import json
 import rspace_client as rc
 import sys
+import json
+import os
 
+# Ask admin to get you the local_conf file
+local_dir = os.path.dirname(__file__)
+with open(os.path.join(local_dir, 'rspace_local_conf.json')) as jsonFile:
+    local_conf = json.load(jsonFile)
 
-test_client = rc.Client('https://rspace.rhrz.uni-bonn.de/', 'NrsJ4NUwtqfsQc1708tV5oduzEoSlISk')
+client = rc.Client(local_conf['server'], local_conf['apiKey'])
+
+print(client.get_status())
+
+client.get_document('SD37310')
