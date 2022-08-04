c9bcde5f8f16d0210b6b597ddd87c0b5f062fa30
Chris Broz <CBroz@datajoint.com>
2022-08-04 11:42:31 -0500

    WIP: bpod class refactor

--- adamacs/ingest/bpod.py
+++ adamacs/ingest/bpod.py
@@ -12,17 +12,19 @@ from ..paths import get_experiment_root_data_dir
 logger = logging.getLogger("datajoint")
 
 
-class BPodFile:
+class Bpodfile(object):
     def __init__(self, bpod_path):
         self.bpod_path = Path(bpod_path)
         if not self.bpod_path.exists():
-            bpod_path_full = find_full_path(get_experiment_root_data_dir(), bpod_path)
+            self._bpod_path_full = find_full_path(
+                get_experiment_root_data_dir(), bpod_path
+            )
         else:
-            bpod_path_full = Path(bpod_path)
+            self._bpod_path_full = Path(bpod_path)
 
         # NOTE: Daniel made a comment that np.squeeze did work on singleton dimensions
         #       returning empty array. Chris couldn't find the same issue
-        self._raw_data = spio.loadmat(bpod_path_full, simplify_cells=True)
+        self._raw_data = spio.loadmat(self._bpod_path_full, simplify_cells=True)
 
     @property
     def start_time(self):  # bpod file creation time in UTC
@@ -53,72 +55,79 @@ class BPodFile:
     def trial_data(self):
         return self.session_data["RawEvents"]["Trial"]
 
-    class Trial:
-        def __init__(self, idx):
-            self._idx = idx
-            self._parent = UNKNOWN  # TODO: figure out calling parent
+    def trial(self, idx):
+        return Trial(idx, self._bpod_path_full, self.session_data, self.trial_data)
 
-        @property
-        def name(self):
-            return self._parent.session_data["TrialTypeNames"][self._idx]
 
-        @property
-        def start(self):
-            return self._parent.session_data["TrialStartTimestamp"][self._idx]
+class Trial(object):
+    def __init__(self, idx, bpod_path_full, session_data=None, trial_data=None):
+        if not session_data:
+            session_data = Bpodfile(bpod_path_full).session_data
+        if not trial_data:
+            trial_data = Bpodfile(bpod_path_full).trial_data
+        self._idx = idx
+        self._bpod_path_full = bpod_path_full
+        self._session_data = session_data
+        self._trial_data = trial_data
 
-        @property
-        def end(self):
-            return self._parent.session_data["TrialEndTimestamp"][self._idx]
+    @property
+    def name(self):
+        return self._session_data["TrialTypeNames"][self._idx]
 
-        @property
-        def states(self):
-            states_dict = self._parent.trial_data[self._idx]["States"]
-            # Filter out states with all nan values:
-            return {k: v for k, v in states_dict.items() if not np.all(np.isnan(v))}
+    @property
+    def start(self):
+        return self._session_data["TrialStartTimestamp"][self._idx]
 
-        @property
-        def events(self):
-            return self._parent.trial_data[self._idx]["Events"]
+    @property
+    def end(self):
+        return self._session_data["TrialEndTimestamp"][self._idx]
 
-        @property
-        def time_to_port(self):
-            return self.states.get("WaitForResponse")
+    @property
+    def states(self):
+        states_dict = self._trial_data[self._idx]["States"]
+        # Filter out states with all nan values:
+        return {k: v for k, v in states_dict.items() if not np.all(np.isnan(v))}
 
-        @property
-        def cue_delay(self):
-            return self.states.get("CueDelay")
+    @property
+    def events(self):
+        return self._trial_data[self._idx]["Events"]
 
-        @property
-        def drinking(self):
-            return self.states.get("Drinking")
+    @property
+    def time_to_port(self):
+        return self.states.get("WaitForResponse")
 
-        @property
-        def reward(self):  # following beacon_pokes_plot.m, only first trial
-            return (
-                self.time_to_port
-                + self.session_data["TrialSettings"][0]["GUI"]["RewardDelay"]
-            )
+    @property
+    def cue_delay(self):
+        return self.states.get("CueDelay")
 
-        @property  # NOTE: assumes one port in event per trial
-        def port_in(self):
-            """Returns number and timestamp of input port"""
-            ports_in = [port for port in self.events if "In" in port]
-            assert len(ports_in) == 1, (
-                "Code assumes only one port-in event per trial.\n"
-                + f"Please refactor and test with {self.bpod_path.stem}"
-            )
-            return ports_in[0][4:-2], self.events[ports_in[0]]
+    @property
+    def drinking(self):
+        return self.states.get("Drinking")
 
-        @property
-        def error(self):
-            return True if "Punish" in self.states else False
+    @property
+    def reward(self):  # following beacon_pokes_plot.m, only first trial
+        return (
+            self.time_to_port
+            + self._session_data["TrialSettings"][0]["GUI"]["RewardDelay"]
+        )
+
+    @property  # TODO: Can we assume one port_in event per trial?
+    def port_in(self):
+        """Returns list of tuples: number and timestamp of input port"""
+        ports_in = [  # tuple of port# and timestamp
+            (int(port[4:-2]), self.events[port]) for port in self.events if "In" in port
+        ]
+        return ports_in if ports_in else None
+
+    @property
+    def error(self):
+        return True if "Punish" in self.states else False
 
 
 # --------------------- HELPER LOADER FUNCTIONS -----------------
 
-
-# matlab script exact translation. Makes tuple: for any port with 'in'.
-# Not needed abov e
+# matlab script exact translation by request
+# Depreciated by Trial.port_in above
 def PortInEvents(bpod_session, idx):
     """Replicate MATLAB func: return list of tuples for input ports: #, events, name"""
     events = bpod_session["RawEvents"]["Trial"][idx]["Events"]
