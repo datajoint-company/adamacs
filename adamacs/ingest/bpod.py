65764b6895dce795b55edf9f3360b5e88ab22e01
Chris Broz <CBroz@datajoint.com>
2022-08-08 17:24:33 -0500

    BPod draft plus notebook

--- adamacs/ingest/bpod.py
+++ adamacs/ingest/bpod.py
@@ -1,14 +1,11 @@
-import logging
 import numpy as np
 import datajoint as dj
 import scipy.io as spio
 from pathlib import Path
 from dateutil import parser
-from element_interface.utils import find_full_path
-from ..pipeline import trial, event, session
+from element_interface.utils import find_full_path, find_root_directory
+from ..pipeline import subject, session, trial, event
 from ..paths import get_experiment_root_data_dir
-
-logger = logging.getLogger("datajoint")
 
 
 class Bpodfile(object):
@@ -20,35 +17,17 @@ class Bpodfile(object):
             )
         else:
             self._bpod_path_full = Path(bpod_path)
-
-        # NOTE: Daniel made a comment that np.squeeze did work on singleton dimensions
-        #       returning empty array. Chris couldn't find the same issue
-        self._raw_data = spio.loadmat(self._bpod_path_full, simplify_cells=True)
-
-    @property
-    def start_time(self):  # bpod file creation time in UTC
-        return parser.parse(
-            str(self._raw_data["__header__"]).split("Created on:")[-1][1:-1]
+        self._bpod_path_relative = self._bpod_path_full.relative_to(
+            find_root_directory(get_experiment_root_data_dir(), self._bpod_path_full)
         )
+
+        # NOTE: Daniel made a comment that np.squeeze didn't work on singleton
+        #       dimensions, returning empty array. Chris couldn't find the same issue
+        self._raw_data = spio.loadmat(self._bpod_path_full, simplify_cells=True)
 
     @property
     def session_data(self):
         return self._raw_data["SessionData"]
-
-    @property
-    def bpod_version(self):
-        return self.session_data["Info"]["StateMachineVersion"].split(" ")[-1]
-
-    @property
-    def recording_duration(self):  # In seconds
-        return sum(
-            self.session_data["TrialEndTimestamp"]
-            - self.session_data["TrialStartTimestamp"]
-        )
-
-    @property
-    def n_trials(self):
-        return self.session_data["nTrials"]
 
     @property
     def trial_data(self):
@@ -57,20 +36,128 @@ class Bpodfile(object):
     def trial(self, idx):
         return Trial(idx, self._bpod_path_full, self.session_data, self.trial_data)
 
-    def ingest(self):
+    def ingest(self, prompt=True):
+        # ------------------------------- Some constants -------------------------------
+        subject_id = self.session_data["CurrentSubjectName"]
+        session_id = (  # incriment previous session id by 1
+            dj.U().aggr(session.Session, n="max(session_id)").fetch("n") or 0
+        ) + 1
+        start_time = parser.parse(
+            str(self._raw_data["__header__"]).split("Created on:")[-1][1:-1]
+        )
+        bpod_version = self.session_data["Info"]["StateMachineVersion"].split(" ")[-1]
+        n_trials = self.session_data["nTrials"]
+
+        # ------------------------------- Keys to insert -------------------------------
         session_key = {
-            "session_id": (
-                dj.U().aggr(session.Session, n="max(session_id)").fetch("n") or 0
-            )
-            + 1,  # incriment previous session id by 1
-            "subject": self.session_data["CurrentSubjectName"],
-            "session_datetime": self.start_time,
+            "session_id": session_id,
+            "subject": subject_id,
+            "session_datetime": start_time,
         }
+        behavior_recording_key = {
+            "session_id": session_id,
+            "recording_start_time": start_time,
+            "recording_duration": sum(
+                # removes time between trials, following example matlab code
+                self.session_data["TrialEndTimestamp"]
+                - self.session_data["TrialStartTimestamp"]
+            ),
+            "recording_notes": f"BPod version: {bpod_version}",
+            # TODO: add self.session_data['Notes']? is it used?
+        }
+        behavior_recording_fp_key = {
+            "session_id": session_id,
+            "filepath": self._bpod_path_relative,
+        }
+        trial_type_keys = [
+            {
+                "trial_type": trial_type
+                for trial_type in np.unique(
+                    self.session_data["TrialTypeNames"]
+                ).tolist()
+            }
+        ]
+        trial_keys = [
+            {
+                "session_id": session_id,
+                "trial_id": n,
+                "trial_type": self.trial(n).type,
+                "trial_start_time": self.trial(n).start,
+                "trial_stop_time": self.trial(n).end,
+            }
+            for n in range(n_trials)
+        ]
+        trial_attributes_keys = [
+            {
+                "session_id": session_id,
+                "trial_id": n,
+                "attribute_name": attrib,
+                "attribute_value": self.trial(n).attributes[attrib],
+            }
+            for n in range(n_trials)
+            for attrib in self.trial(n).attributes
+            if self.trial(n).attributes[attrib]
+        ]
+        event_type_keys = [
+            {"event_type": event_type}
+            for event_type in set(
+                event_type
+                for n in range(n_trials)
+                for event_type in self.trial(n).events
+            )
+        ]
+        event_keys = [
+            {
+                "session_id": session_id,
+                "trial_id": n,
+                "event_type": event,
+                "event_start_time": self.trial(n).start + event_start,
+            }
+            for n in range(n_trials)
+            for event, event_start in self.trial(n).events.items()
+            if event_start
+        ]
+
+        # ---------------------------------- Prompt ----------------------------------
+        print(
+            "\n\t".join(
+                [
+                    "BPod items to be inserted:",
+                    f"Subject : {subject_id}",
+                    f"Time    : {start_time}",
+                    f"N Trials: {n_trials}",
+                    f"N Events: {len(event_keys)}",
+                ]
+            )
+        )
+        if (
+            prompt
+            and dj.utils.user_choice("Proceed with new subject(s) insert?") != "yes"
+        ):
+            print("Canceled insert.")
+            return
+
+        # ----------------------------- Insert to schemas -----------------------------
+        if not subject.Subject & f'subject="{subject_id}"':
+            from .pyrat import PyratIngestion
+
+            print(f"Subject does not yet exist. Attempting pyrat import: {subject_id}")
+            PyratIngestion().ingest_animal(subject_id)
 
         with session.Session.connection.transaction:
-            session.insert1(session_key)
-            # event.insert()
-            # trial.insert()
+            session.Session.insert1(session_key, skip_duplicates=True)  # remove skip
+            event.BehaviorRecording.insert1(behavior_recording_key)
+            event.BehaviorRecording.File.insert1(behavior_recording_fp_key)
+            trial.TrialType.insert(trial_type_keys, skip_duplicates=True)
+            trial.Trial.insert(trial_keys, allow_direct_insert=True)
+            trial.Trial.Attribute.insert(
+                trial_attributes_keys, allow_direct_insert=True
+            )
+            event.EventType.insert(event_type_keys, skip_duplicates=True)
+            event.Event.insert(
+                event_keys, allow_direct_insert=True, ignore_extra_fields=True
+            )  # ignore extra trial_id
+            trial.TrialEvent.insert(event_keys, allow_direct_insert=True)
 
 
 class Trial(object):
@@ -79,13 +166,38 @@ class Trial(object):
             session_data = Bpodfile(bpod_path_full).session_data
         if not trial_data:
             trial_data = Bpodfile(bpod_path_full).trial_data
+
+        # internal properties for args above
         self._idx = idx
         self._bpod_path_full = bpod_path_full
         self._session_data = session_data
         self._trial_data = trial_data
 
+        # clean up bpod states
+        self._states = {
+            k: v
+            for k, v in self._trial_data[self._idx]["States"].items()
+            # Filter out states with nan values:
+            if not np.any(np.isnan(v))
+        }  # [None] below makes it easier to differentiate values from returned None
+        self._resp_delay = self._states.get("WaitForResponse", [None])
+        if self._resp_delay[0] and np.isnan(self._resp_delay[0]):
+            self._resp_delay = [None]  # sometimes _resp_delay is [nan, nan]
+        self._time_to_port = (
+            self._resp_delay[1] - self._resp_delay[0] if self._resp_delay[0] else None
+        )
+
+        # clean up bpod port events
+        self._raw_events = self._trial_data[self._idx]["Events"]
+        ports_in = [port for port in self._raw_events if "In" in port]
+        assert len(ports_in) <= 1, (
+            "BPod ingestion assumes up to 1 PortIn per trial. Please refactor for trial"
+            + f"{self._idx} of\n{self._bpod_path_full.name}"
+        )
+        self._port_in = ports_in[0] if ports_in else None
+
     @property
-    def name(self):
+    def type(self):
         return self._session_data["TrialTypeNames"][self._idx]
 
     @property
@@ -93,80 +205,52 @@ class Trial(object):
         return self._session_data["TrialStartTimestamp"][self._idx]
 
     @property
-    def end(self):
+    def end(self):  # in element, stop_time
         return self._session_data["TrialEndTimestamp"][self._idx]
 
     @property
-    def states(self):
-        states_dict = self._trial_data[self._idx]["States"]
-        # Filter out states with all nan values:
-        return {k: v for k, v in states_dict.items() if not np.all(np.isnan(v))}
+    def attributes(self):
+        """Returns all attributes for trial.Trial.Attributes as a dict"""
+        return {
+            "port_num": self._port_in[4:-2] if self._port_in else None,
+            "error": True if "Punish" in self._states else False,
+            "timeout": (
+                True
+                if self._time_to_port
+                and self._time_to_port
+                >= self._session_data["TrialSettings"][self._idx]["GUI"]["ResponseTime"]
+                else False
+            ),
+        }
 
     @property
     def events(self):
-        return self._trial_data[self._idx]["Events"]
-
-    @property
-    def _time_to_list(self):
-        return self.states.get("WaitForResponse")
-
-    @property
-    def time_to_port(self):
-        if len(self._time_to_list) > 1:  # Sometimes this is None
-            return self._time_to_list[1] - self._time_to_list[0]
-
-    @property
-    def time_to_target(self):
-        return self._time_to_list[0] if self._time_to_list else None
-
-    @property
-    def cue_delay(self):  # first item is 'Trigger Zone entry'. Ignore second?
-        # Below, [None] avoids TypeErr if no CueDelay
-        return self.states.get("CueDelay", [None])[0]
-
-    @property
-    def drinking(self):
-        return self.states.get("Drinking")
-
-    @property
-    def reward(self):  # following beacon_pokes_plot.m, only first trial
-        return (
-            self.time_to_port
-            + self._session_data["TrialSettings"][0]["GUI"]["RewardDelay"]
-        )
-
-    @property  # TODO: Can we assume one port_in event per trial?
-    def port_in(self):
-        """Returns list of tuples: number and timestamp of input port"""
-        ports_in = [  # tuple of port# and timestamp
-            (int(port[4:-2]), self.events[port]) for port in self.events if "In" in port
-        ]
-        return ports_in if ports_in else None
-
-    @property
-    def error(self):
-        return True if "Punish" in self.states else False
-
-    @property
-    def timeout(self):
-        return (
-            True
-            if self.time_to_port
-            >= self._session_data["TrialSettings"][self._idx]["GUI"]["ResponseTime"]
-            else False
-        )
+        """Returns trial events as a dict {event_type: event_time} WRT trial start"""
+        return {
+            "cue": self._states.get("CueDelay", [None])[0],
+            "at_target": self._resp_delay[0] if self._resp_delay[0] else None,
+            "at_port": self._time_to_port,
+            "reward": (
+                self._time_to_port
+                + self._session_data["TrialSettings"][0]["GUI"]["RewardDelay"]
+                if self._time_to_port
+                else None
+            ),
+            "in_port": self._raw_events[self._port_in] if self._port_in else None,
+            "drinking": self._states.get("Drinking", [None])[0],
+        }
 
 
 # --------------------- HELPER LOADER FUNCTIONS -----------------
 
-# matlab script exact translation by request
-# Depreciated by Trial.port_in above
+# matlab script exact translation
+# Depreciated by Trial attribute/event split above: port_num vs at_port time
 def PortInEvents(bpod_session, idx):
     """Replicate MATLAB func: return list of tuples for input ports: #, events, name"""
     events = bpod_session["RawEvents"]["Trial"][idx]["Events"]
     in_ports = [f for f in events.keys() if "In" in f]
-    in_port_events = []
+    in_port_raw_events = []
     for port in in_ports:
         # (port#, event times, portname)
-        in_port_events.append((port[4:-2], events[port], port))
-    return in_port_events
+        in_port_raw_events.append((port[4:-2], events[port], port))
+    return in_port_raw_events
---
+++ notebooks/py_scripts/09_bpod_insert.py
@@ -0,0 +1,84 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.7
+#   kernelspec:
+#     display_name: Python 3.8.2 ('bonn')
+#     language: python
+#     name: python3
+# ---
+
+# # PyRAT subject ingestion
+
+# ## Setup
+
+# Using local config file (see [01_pipeline](./01_pipeline.ipynb))
+
+import os
+# change to the upper level folder to detect dj_local_conf.json
+if os.path.basename(os.getcwd())=='notebooks': os.chdir('..')
+assert os.path.basename(os.getcwd())=='adamacs', ("Please move to the main directory")
+from adamacs.pipeline import subject
+
+# Manual entry
+
+# Manual Entry
+import datajoint as dj; import getpass
+dj.config['database.host'] = '172.26.128.53'        # Put the server name between these apostrophe
+dj.config['database.user'] = 'danielmk'             # Put your user name between these apostrophe
+dj.config['database.password'] = getpass.getpass(prompt='Database password:')
+dj.conn()
+
+# ## Imports and Root Path Setup
+
+# Your `dj.config` file should have a section for your BPod root directory under `custom`: `exp_root_data_dir`. This is a list of one or more paths where the ingestion tool will look for the relative paths it is given.
+
+# +
+import datajoint as dj
+from element_interface.utils import find_full_path
+from adamacs.pipeline import session, event, trial
+from adamacs.ingest.bpod import Bpodfile
+from adamacs.paths import get_experiment_root_data_dir
+
+bpod_path = "scan9FB2LN5C_WEZ-8701_StimArenaMaster_20220318_165447.mat"
+root_dirs = dj.config["custom"]["exp_root_data_dir"]
+bpod_path_full = find_full_path(get_experiment_root_data_dir(),bpod_path)
+
+print(f"Root: {root_dirs}\nFull: {bpod_path_full}")
+# -
+
+# ## Initial check of tables
+
+# +
+from adamacs.pipeline import session, event, trial
+
+session.Session.delete()
+print('Sessions:', len(session.Session()))
+print('Trials  :', len(trial.Trial()))
+print('Events  :', len(event.Event()))
+# -
+
+# ## Automated ingestion
+
+# The function is designed ask for a confirmation before entered into the schema.
+
+bpod_path = "scan9FB2LN5C_WEZ-8701_StimArenaMaster_20220318_165447.mat"
+bpod_object = Bpodfile(bpod_path)
+bpod_object.ingest()
+
+# Check that insertion worked:
+
+trial.TrialEvent & 'trial_id=1'
+
+# We can also interact with bpod objects. For example:
+
+bpod_object.trial(1).attributes
+
+bpod_object.trial(1).events
+
+
